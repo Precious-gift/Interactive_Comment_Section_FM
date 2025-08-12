27468ac400e0ab9ac4e7dd0f4132067c5ac24b2a
Precious-gift <falayeprecious@gmail.com>
2025-08-12 15:30:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="rubik-font min-h-screen p-24 flex justify-center items-center">
+      <h1 className="text-4xl">Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
